4ff55dab029aa6bd704ffc26b83846a16b867ad4
5Runi <rgeldner@pm.me>
2026-03-28 10:48:06 -0400

    trying to run

--- challenges/fizzbuzz.ts
+++ challenges/fizzbuzz.ts
@@ -12,28 +12,33 @@ fizzbuzz(16);
 
 */
 
-const fizzbuzz = (num: number): void => {
+const fizzbuzz = (num: number): (number | string)[] => {
   let results: (number | string)[] = [];
-  //let input = num -- to 1//need to decrement to 1
-  for (let i = 1; i < num;  i--) {
+  //start at 1, increment up to and include num
+  for (let i = 1; i <= num; i++) {
     if (i % 5 === 0 && i % 3 === 0) results.push("fizzbuzz");
     else if (i % 5 === 0) results.push("buzz");
     else if (i % 3 === 0) results.push("fizz");
-    else results.push(i)
-    return results;
+    else results.push(i);
   }
-  console.log(fizzbuzz(36));
+  return results;
 };
+
+console.log(fizzbuzz(36));
 
 /*
 
 Extension: 
 Write a function that returns an array containing the numbers 1 to NUM.
 
-Put "fizz" in place of numbers divisible by 3 but not by 5 or 7,
-"buzz" in place of numbers divisible by 5 but not by 3 or 7,
-"bazz" in place of numbers divisible by 7 but not by 3 or 5,
-"fizzbuzz" in place of numbers divisible by 3 and 5 but not 7,
+//Initialize an array to hold the results
+
+Put "fizz" in place of numbers divisible by 3 but not by 5 or 7,//as in 15, should NOT be included? 
+%3===0 && %5... no. see have to catch it in an if else block, and if it trickles through.... so will work from the bottom up
+"buzz" in place of numbers divisible by 5 but not by 3 or 7,//hhhmmmm
+"bazz" in place of numbers divisible by 7 but not by 3 or 5,//good gravy
+"fizzbuzz" in place of numbers divisible by 3 and 5 but not 7,//ok, well its just the 2 words appended, so I am tempted to use || or &&
+and i guess i could append them or i could exclude them regardless of the words
 "fizzbazz" in place of numbers divisible by 3 and 7 but not 5,
 "buzzbazz" in place of numbers divisible by 5 and 7 but not 3,
 and "fizzbuzzbazz" in place of numbers divisible by 3, 5, and 7.
@@ -45,8 +50,8 @@ fizzbuzzbazz(25);
 
 */
 
-const fizzbuzzbazz = (num: number): (number | string)[] => {
-  return [];
-};
+// const fizzbuzzbazz = (num: number): (number | string)[] => {
+//   return [];
+// };
 
-export { fizzbuzz, fizzbuzzbazz };
+// export { fizzbuzz, fizzbuzzbazz };
